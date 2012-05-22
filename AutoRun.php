f9a28dececac90c183c688281f5f25d5060fe2b5
Eher <alexandre.eher@apontador.com>
2012-05-22 15:40:20 -0300

    Nofity test results via growlnotify

--- AutoRun.php
+++ AutoRun.php
@@ -1,14 +1,16 @@
 <?php
+define(PATH_IMAGE_GREEN, __DIR__ . '/green.png');
+define(PATH_IMAGE_RED, __DIR__ . '/red.png');
 
 class AutoRun {
     static public $lastTime;
-    
+
     public static function main($argc, array $argv = array()) {
         chdir(".");
         $lastTime = time();
         $dir      = new DirectoryIterator(".");
         $autorun  = new AutoRun($lastTime, $dir);
-        
+
         if ($argc <= 1) {
             self::usage();
             exit(1);
@@ -21,7 +23,7 @@ class AutoRun {
             $autorun->run($command);
         }
     }
-    
+
     public static function usage() {
         echo "
 AutoRun 1.0.0 by Marcello Duarte.
@@ -32,14 +34,14 @@ and it has the followig line:
 php AutoRun.php <command>
 
 Usage: autorun
-";
+            ";
     }
-    
+
     public function __construct($lastTime, DirectoryIterator $testDir) {
         self::$lastTime = $lastTime;
         $this->testDir = $testDir;
     }
-    
+
     public function run($command) {
         foreach ($this->testDir as $file){
             if ($file->isDot()) continue;
@@ -48,20 +50,22 @@ Usage: autorun
                 $this->recursivelyRun($file, $command);
                 continue;
             }
-            
-            $this->runCommandIfThisFileWasModified($file, $command);
+
+            if (preg_match('/\.php$/', $file)) {
+                $this->runCommandIfThisFileWasModified($file, $command);
+            }
         }
     }
-    
+
     /**
      * For some reason I can't use the same Directory Iterator object. I needed
      * a brand new Directory Iterator due to some internals of how the SPL class works
      */
     private function recursivelyRun(DirectoryIterator $dir, $command) {
         $this->cloneDirectoryIteratorAndCreateNewAutoRun($dir)
-             ->run($command);
+            ->run($command);
     }
-    
+
     /**
      * Cloning with the clone statement wasn't enough. I really need a new object.
      * I create one with from the previous directory's path. 
@@ -70,23 +74,34 @@ Usage: autorun
         $clone = new DirectoryIterator($dir->getPathName());
         return new AutoRun(self::$lastTime, $clone);
     }
-    
+
     private function runCommandIfThisFileWasModified(SplFileInfo $file, $command) {
         if ($this->wasModifiedSinceLastRun($file)) {
             $this->clearTerminalAndRun($command);
             $this->updateLastModifiedTime($file);
         }
     }
-    
+
     private function wasModifiedSinceLastRun(SplFileInfo $file) {
         return filemtime($file->getRealpath()) > self::$lastTime;
     }
-    
+
     private function clearTerminalAndRun($command) {
         system('clear');
-        system($command);
+        system($command, $result);
+        $this->notifyResult($result);
     }
-    
+
+    private function notifyResult($result)
+    {
+        $hasError = (bool) $result;
+        if ($hasError) {
+            system('growlnotify -m "Fail" -t "PHPUnit" --image ' . PATH_IMAGE_RED);
+        } else {
+            system('growlnotify -m "Pass" -t "PHPUnit" --image ' . PATH_IMAGE_GREEN);
+        }
+    }
+
     private function updateLastModifiedTime(SplFileInfo $modifiedFile) {
         self::$lastTime = $modifiedFile->getMTime();
     }
